da487558070f4bd5ef7fb5a33f1cbbfff18854a4
LuciaWesters <79092031+LuciaWesters@users.noreply.github.com>
2021-02-27 11:06:59 +0100

    Javascript

---
+++ scripts/progress.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('.chart').easyPieChart({
+    size: 170,
+    barColor: "#80bfff",
+    scaleLength: 0,
+    lineWidth: 10,
+    trackColor: "black",
+    lineCap: "circle",
+    animate: 2000,
+  });
+});
+
+
+
+
+//https://codepen.io/AliReza10/pen/abzmggX//
